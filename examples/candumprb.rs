050ef5aa029f6e16bec213625326160207472d8b
marcelbuesing <buesing.marcel@googlemail.com>
2020-10-01 11:36:14 +0200

    Add dbc decoding

--- examples/candumprb.rs
+++ examples/candumprb.rs
@@ -1,13 +1,16 @@
-use ansi_term::Color;
-use ansi_term::Color::Fixed;
-
+use ansi_term::Color::{self, Cyan, Fixed, Green, Purple};
+use anyhow::Result;
+use can_dbc::{ByteOrder, Signal};
 use futures::prelude::*;
 use futures_util::compat::Stream01CompatExt;
-
+use socketcan::CANFrame;
+use std::collections::HashMap;
+use std::convert::TryInto;
 use std::fmt::Write;
 use std::path::PathBuf;
-
 use structopt::StructOpt;
+use tokio::fs::File;
+use tokio::io::AsyncReadExt;
 use tokio_socketcan;
 
 const COLOR_CAN_ID: Color = Color::White;
@@ -82,9 +85,9 @@ impl Byte {
     about = "Candump Rainbow. A colorful can dump tool with dbc support."
 )]
 struct Opt {
-    /// DBC Input file
-    #[structopt(short = "f", long = "dbc-file", parse(from_os_str))]
-    dbc_file: Option<PathBuf>,
+    /// DBC file path, if not passed frame signals are not decoded
+    #[structopt(short = "i", long = "input", parse(from_os_str))]
+    input: Option<PathBuf>,
 
     /// Set can interface
     #[structopt(help = "socketcan CAN interface e.g. vcan0")]
@@ -92,7 +95,7 @@ struct Opt {
 }
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> Result<()> {
     let opt = Opt::from_args();
 
     let mut socket_rx = tokio_socketcan::CANSocket::open(&opt.can_interface)
@@ -106,9 +109,33 @@ async fn main() -> std::io::Result<()> {
         })
         .collect();
 
+    // Read DBC and turn it into a hashmap for lookup
+    let signal_lookup = if let Some(dbc_input) = opt.input.as_ref() {
+        let mut f = File::open(dbc_input).await?;
+        let mut buffer = Vec::new();
+        f.read_to_end(&mut buffer).await?;
+        let dbc = can_dbc::DBC::from_slice(&buffer).expect("Failed to parse DBC");
+
+        let mut signal_lookup = HashMap::new();
+
+        for msg in dbc.messages() {
+            signal_lookup.insert(
+                msg.message_id().0 & !socketcan::EFF_FLAG,
+                (msg.message_name().clone(), msg.signals().clone()),
+            );
+        }
+        Some(signal_lookup)
+    } else {
+        None
+    };
+
     while let Some(socket_result) = socket_rx.next().await {
         match socket_result {
             Ok(frame) => {
+                if let Some(signal_lookup) = signal_lookup.as_ref() {
+                    print_dbc_signals(signal_lookup, &frame);
+                }
+
                 let mut buffer: String = String::new();
 
                 if frame.is_extended() {
@@ -121,8 +148,7 @@ async fn main() -> std::io::Result<()> {
                     buffer,
                     "{}",
                     COLOR_CAN_ID.paint(format!("{:08x} ", frame.id()))
-                )
-                .unwrap();
+                )?;
 
                 for b in frame.data() {
                     write!(buffer, "{}", byte_hex_table[*b as usize]).unwrap();
@@ -137,4 +163,38 @@ async fn main() -> std::io::Result<()> {
     }
 
     Ok(())
+}
+
+// Given a CAN Frame, lookup the can signals and print the signal values
+fn print_dbc_signals(signal_lookup: &HashMap<u32, (String, Vec<Signal>)>, frame: &CANFrame) {
+    let id = frame.id() & !socketcan::EFF_FLAG;
+    let (message_name, signals) = signal_lookup.get(&id).expect("Unknown message id");
+    println!("\n{}", Purple.paint(message_name));
+
+    for signal in signals.iter() {
+        let frame_data: [u8; 8] = frame
+            .data()
+            .try_into()
+            .expect("slice with incorrect length");
+
+        let signal_value: u64 = if *signal.byte_order() == ByteOrder::LittleEndian {
+            u64::from_le_bytes(frame_data)
+        } else {
+            u64::from_be_bytes(frame_data)
+        };
+
+        // Calculate signal value
+        let bit_mask: u64 = 2u64.pow(*signal.signal_size() as u32) - 1;
+        let signal_value = ((signal_value >> signal.start_bit()) & bit_mask) as f32
+            * *signal.factor() as f32
+            + *signal.offset() as f32;
+
+        let signal_value_s = format!("{:6.4}", signal_value);
+
+        println!(
+            "{} → value {}",
+            Green.paint(signal.name()),
+            Cyan.paint(signal_value_s)
+        );
+    }
 }
